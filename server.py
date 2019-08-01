464eb6f8ba5d5c29b87a151f8dea33c561cb0b8c
Santhosh Balasa <santhosh.kbr@gmail.com>
2019-08-01 11:38:27 +0530

    Added server file to receive the client messages

---
+++ server.py
@@ -0,0 +1,55 @@
+import sys
+import time
+import click
+import socket
+import logging
+
+# Logger for Verification failures
+logger_verification = logging.getLogger(__name__+"verification")
+verification_handler = logging.FileHandler("verification_failures.log")
+logger_verification.addHandler(verification_handler)
+logger_verification.setLevel(logging.INFO)
+
+# Logger for Checksum failures
+logger_checksum = logging.getLogger(__name__+"checksum")
+checksum_handler = logging.FileHandler("checksum_failures.log")
+logger_checksum.addHandler(checksum_handler)
+logger_checksum.setLevel(logging.INFO)
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+udp_ip = "127.0.0.1"
+
+
+def write_log(log_type, msg, delay):
+    time.sleep(delay)
+    if log_type == "verification":
+        logger_verification.info(msg)
+    else:
+        logger_checksum.info(msg)
+
+
+@click.command()
+@click.option("-p", help="Port to listen UDP packets from")
+@click.option("-d", default='1', help="Delay in seconds for writing to log files")
+@click.option("--binaries", help="Dictionary of {packet_id: binary_path} mappings")
+@click.option("--keys", help="Dictionary of {packet_id: key_file_path} mappings")
+def main(p, d, binaries, keys):
+    sock.bind((udp_ip, int(p)))
+    count = 5
+    while count:
+        data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+        print("----------------START----------------------")
+        print("received message:", data)
+        print("received address", addr)
+        print("----------------END-----------------------\n")
+        count -= 1
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print("Processor exit with: {}".format(e))
+        sys.exit(1)  # exit with error
